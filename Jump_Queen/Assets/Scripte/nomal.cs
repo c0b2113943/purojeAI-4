b16b912ee0710a100bfcf2f548feb864bbc021be
C0B21139_fuse_ryota <c0b2113943@edu.teu.ac.jp>
2023-06-27 02:01:48 +0900

    Library以外

---
+++ Jump_Queen/Assets/Scripte/nomal.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class nomal : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            SceneManager.LoadScene("game");
+        }
+        
+    }
+}
